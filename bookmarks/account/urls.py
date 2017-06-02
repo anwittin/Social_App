f443686d995ffce408f69157603949b59acdaf3c
Tony Wittinger <anwittin@gmail.com>
2017-06-02 16:27:19 -0700

    downgrade Django to book version

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,17 +1,20 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
-    #previous login form
-    #url(r'^login/$', views.user_login, name='login'),
-    #login / logout urls
-    url(r'^login/$', 
-    'django.contrib.auth.views.login', 
-    name='login'),
-    url(r'^logout/$', 
-    'django.contrib.auth.views.logout', 
-    name='logout'),
+    # previous login view
+    # url(r'^login/$', views.user_login, name='login'), 
+    url(r'^$', views.dashboard, name='dashboard'),
+    # login / logout urls
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        name='login'),
+    url(r'^logout/$',
+        'django.contrib.auth.views.logout',
+        name='logout'),
     url(r'^logout-then-login/$', 
-    'django.contrib.auth.views.logout-then-login', 
-    name='logout-then-login'),
+        'django.contrib.auth.views.logout_then_login', 
+        name='logout_then_login'),
 ]
