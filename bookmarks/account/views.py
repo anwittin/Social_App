c43a8df94fe0e4dee688371c44857f58778344ce
Tony Wittinger <anwittin@gmail.com>
2017-06-01 20:36:38 -0700

    update authentication pages

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -14,6 +14,12 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated Successfully')
+                    return HttpResponse('Authenticated '\
+                                        'successfully')
                 else:
                     return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
